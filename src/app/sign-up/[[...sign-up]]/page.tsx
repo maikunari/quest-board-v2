448f62b666e2eb730791ccc92d60b8622e92c6ca
Jerry Albertson <jerry@agentmail.to>
2026-02-16 09:29:21 +0000

    fix: post-login redirect to /quests instead of /

--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -4,6 +4,7 @@ export default function SignUpPage() {
   return (
     <div className="flex items-center justify-center min-h-[80vh]">
       <SignUp
+        forceRedirectUrl="/quests"
         appearance={{
           elements: {
             rootBox: 'mx-auto',
